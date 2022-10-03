f1d5052cd41bbbdee1f2577a1bea4ae40e687059
SmOke11345 <103870835+SmOke11345@users.noreply.github.com>
2022-10-03 11:42:02 +0600

    modified:   assets/css/main.css 	modified:   assets/css/main.css.map 	new file:   assets/css/owl.carousel.min.css 	new file:   assets/css/owl.theme.default.min.css 	modified:   assets/css/scss/main.scss 	new file:   assets/img/shop-carousel/category--img1.svg 	new file:   assets/img/shop-carousel/category--img2.svg 	new file:   assets/img/shop-carousel/category--img3.svg 	new file:   assets/img/shop-carousel/category--img4.svg 	modified:   assets/js/main.js 	new file:   assets/js/owl.carousel.min.js 	modified:   index.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,6 @@
 
 const select = document.querySelectorAll('.select');
 
-console.log(select);
-
 for (var i = 0; i < select.length; i++) {
     select[i].addEventListener('click', function () {
         // remove active class for all elements
@@ -14,3 +12,14 @@ for (var i = 0; i < select.length; i++) {
         this.classList.add('active');
     });
 }
+
+// carousel
+
+$(document).ready(function () {
+    $('.owl-carousel').owlCarousel({
+        nav: true,
+        loop: true,
+        items: 3,
+        dots: false,
+    });
+});
